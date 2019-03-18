b2e15e993dc43e482b407439ca411d765d2fea79
Miesvanderlippe <miesvanderlippe@gmail.com>
2019-03-18 17:04:38 +0100

    Chp 1 | Temperature Table exercise 1.3 and 1.4

--- Chapter 1/temperature_table.c
+++ Chapter 1/temperature_table.c
@@ -11,10 +11,23 @@ int main(void)
 
     fahrenheit = min;
 
+    printf("===== Fahrenheit to Celcius ===== \n");
+
     while(fahrenheit <= max)
     {
         celcius = (5.0 / 9.0) * (fahrenheit - 32.0);
         printf("%3.0f\t%6.1f\n", fahrenheit, celcius);
         fahrenheit = fahrenheit + step;
+    }
+
+    celcius = min;
+
+    printf("\n===== Celcius to Fahrenheit ===== \n");
+
+    while(celcius <= max)
+    {
+        fahrenheit = (celcius / (5.0 / 9.0)) + 32 ;
+        printf("%3.0f\t%6.1f\n", celcius, fahrenheit);
+        celcius = celcius + step;
     }
 }
